aa4e4d7d3f4cd23b476c5d976d1c845c59891794
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-03 15:44:29 +0000

    Task: 0. Create a file

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,1 +1,9 @@
-#!/usr/bin/env bash
+# create a file in /tmp
+
+file { '/tmp/holberton':
+    ensure  => file,
+    mode    => '0744'
+    owner   => 'www-data'
+    group   => 'www-data'
+    content => 'I love Puppet'
+}
