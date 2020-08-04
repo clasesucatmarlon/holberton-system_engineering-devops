eff72c9d967dbdf18512f392bb4b90ed7f6289fa
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-04 10:58:44 -0500

    configurate the client SSH file configuration

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,1 +1,13 @@
-#!/usr/bin/env bash
+# configurate the client SSH file configuration
+
+file_line {'Turn off passwd auth':
+  replace => true,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'PasswordAuthentication no'
+}
+
+file_line {'Declare identity file':
+  replace => true,
+  path    => '/etc/ssh/ssh_config',
+  line    => 'IdentityFile ~/.ssh/holberton'
+}
