2565c05e49cec75b4b07435e59158751bdfea0d6
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-05 10:25:02 -0500

    Added ensure

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,13 +1,15 @@
 # configurate the client SSH file configuration
 
-file_line {'Turn off passwd auth':
-  replace => true,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'PasswordAuthentication no'
+file_line { 'password':
+  ensure => present,
+  path   => '/etc/ssh/ssh_config',
+  match  => '^.?.*PasswordAuthentication yes$',
+  line   => 'PasswordAuthentication no',
 }
 
-file_line {'Declare identity file':
-  replace => true,
-  path    => '/etc/ssh/ssh_config',
-  line    => 'IdentityFile ~/.ssh/holberton'
+file_line { 'identity':
+  ensure => present,
+  line   => 'IdentityFile ~/.ssh/holberton',
+  match  => '^.*IdentityFile ~/.ssh/holberton$',
+  path   => '/etc/ssh/ssh_config',
 }
