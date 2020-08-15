bc15c7f63d8abb136afe532b1430b4890b756ef0
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-08-14 20:56:43 -0500

    Added a custom HTTP header with Puppet

--- 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response-header.pp
@@ -1,12 +1,12 @@
 exec { 'apt-get-update':
   command => '/usr/bin/apt-get update',
 }
- 
+
 package { 'nginx':
   ensure  => installed,
   require => Exec['apt-get-update'],
 }
- 
+
 file_line { 'config redirect':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
@@ -14,7 +14,7 @@ file_line { 'config redirect':
   line    => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4 permanent;',
   require => Package['nginx'],
 }
- 
+
 file_line { 'config name':
   ensure  => 'present',
   path    => '/etc/nginx/sites-available/default',
@@ -22,12 +22,12 @@ file_line { 'config name':
   line    => 'add_header X-Served-By $hostname;',
   require => Package['nginx'],
 }
- 
+
 file { '/var/www/html/index.html':
   content => 'Holberton School',
   require => Package['nginx'],
 }
- 
+
 service { 'nginx':
   ensure  => running,
   require => Package['nginx'],
