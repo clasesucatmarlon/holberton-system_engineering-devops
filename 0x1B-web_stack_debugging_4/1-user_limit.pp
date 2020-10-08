80f262c7f68c2b272110b17dee3964147082ec57
clasesucatmarlon <clasesucatmarlon@gmail.com>
2020-10-08 10:22:22 -0500

    Change the OS configuration so that it is possible to login with yhe holberton

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,12 +1,8 @@
 # Change the OS configuration so that it is possible to login with
 # the holberton user and open a file without any error message.
 
-exec { 'one':
-    command =>  'sed -i "/holberton hard nofile 5/d" /etc/security/limits.conf'
-    path    =>  '/bin'
-}
-
-exec { 'Two':
-    command =>  'sed -i "/holberton soft nofile 4/d" /etc/security/limits.conf'
-    path    =>  '/bin'
+file { 'remove user list':
+    ensure  =>  file,
+    patch   =>  '/etc/security/limits.conf',
+    content =>  '\n'
 }
